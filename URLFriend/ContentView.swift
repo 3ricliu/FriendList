08bb97d382e8b839e7cb64eb45232af8542258f5
Eric Liu <ericliu4943@gmail.com>
2020-11-12 02:06:52 -0800

    Fetching from web, parsing list view correctly

--- URLFriend/ContentView.swift
+++ URLFriend/ContentView.swift
@@ -7,15 +7,66 @@
 
 import SwiftUI
 
+struct User: Codable {
+  let id: String?
+  let isActive: Bool?
+  let name: String?
+  let age: Int?
+  let company: String?
+  let email: String?
+  let address: String?
+  let about: String?
+  let registered: String?
+  let tags: [String?]
+  let friends: [Friend?]
+}
+
+struct Friend: Codable {
+  let id: String?
+  let name: String?
+}
+
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var users = [User]()
+
+  var body: some View {
+    NavigationView {
+      List(users, id: \.id) { user in
+        NavigationLink(destination: Text("\(user.id ?? "ay")")) {
+          Text("\(user.name ?? "boo")")
+        }
+      }
     }
+    .onAppear(perform: loadData)
+  }
+
+  func loadData() {
+    guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+      print("Invalid URL")
+      return
+    }
+    
+    let request = URLRequest(url: url)
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+      if let data = data {
+        if let decodedResponse = try? JSONDecoder().decode([User].self, from: data) {
+          DispatchQueue.main.async {
+            self.users = decodedResponse
+          }
+          
+          return
+        }
+      }
+      
+      print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+    }.resume()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
