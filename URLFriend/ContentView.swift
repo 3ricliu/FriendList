95f75fe05654da262add5d2f9bd8830640accfc9
Eric Liu <ericliu4943@gmail.com>
2020-11-11 22:38:53 -0800

    Initial Commit

---
+++ URLFriend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  URLFriend
+//
+//  Created by Eric Liu on 11/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ URLFriend/URLFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLFriendApp.swift
+//  URLFriend
+//
+//  Created by Eric Liu on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct URLFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
